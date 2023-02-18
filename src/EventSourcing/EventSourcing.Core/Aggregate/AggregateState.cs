55cab6a0abea89723f894c5af8ac5b1a149faa8a
Philip Oterdahl <tintin.oterdahl@hotmail.com>
2023-02-18 13:45:30 +0100

    Test aggregates using given when then

--- src/EventSourcing/EventSourcing.Core/Aggregate/AggregateState.cs
+++ src/EventSourcing/EventSourcing.Core/Aggregate/AggregateState.cs
@@ -3,6 +3,4 @@
 public abstract record AggregateState
 {
     public string StreamId { get; init; }
-    public DateTime? Created { get; init; }
-    public DateTime? LastEventAppeared { get; init; }
 }
---
+++ src/EventSourcing/Tests/EventSourcing.Core.Tests/Aggregate/AggregateTestBase.cs
@@ -0,0 +1,85 @@
+﻿using EventSourcing.Core.Aggregate;
+using EventSourcing.Core.EventStore;
+
+namespace EventSourcing.Core.Tests.Aggregate;
+
+public class AggregateTestBase<TId, TState, TAggregate>
+    where TAggregate : Aggregate<TId, TState>, new()
+    where TId : StreamId, IStringToStreamIdConverter<TId>
+    where TState : AggregateState, new()
+{
+    private TAggregate AggregateRoot { get; set; }
+    private int index = 0;
+
+    protected void Given(params EventStoreEvent[] events)
+    {
+        AggregateRoot = new TAggregate();
+        AggregateRoot.Load(events);
+    }
+
+    protected void Given()
+    {
+        // this function is only meant to be used to signal that there is no setup
+    }
+    
+    protected void When(Func<TAggregate> createCommand)
+    {
+        AggregateRoot = createCommand();
+    }
+
+    protected void When(Action<TAggregate> command)
+    {
+        command(AggregateRoot);
+    }
+
+    protected void Then<TEvent>(params Action<TEvent>[] conditions) 
+        where TEvent : EventStoreEvent
+    {
+        var events = AggregateRoot.GetUncommittedEvents();
+        
+        var @event = events.ElementAt(index);
+
+        if (@event is null)
+            throw new InvalidOperationException("No event produced");
+
+        if (@event is not TEvent testEvent)
+            throw new InvalidOperationException(
+                $"Event produces is not of expected type: {typeof(TEvent).Name} but of type: {@event.GetType().Name}"
+            );
+            
+        foreach (var condition in conditions)
+        {
+            condition(testEvent);
+        }
+
+        index++;
+    }
+
+    protected void Then(params Action<TState>[] conditions)
+    {
+        foreach (var condition in conditions)
+        {
+            condition(AggregateRoot);
+        }
+    }
+    
+    protected void Throws<TException>(Func<TAggregate> command, params Action<TException>[] conditions) where TException : Exception
+    {
+        var exception = Assert.Throws<TException>(command);
+
+        foreach (var condition in conditions)
+        {
+            condition(exception);
+        }
+    }
+
+    protected void Throws<TException>(Action<TAggregate> command, params Action<TException>[] conditions) where TException : Exception
+    {
+        var exception = Assert.Throws<TException>(() => command(AggregateRoot));
+
+        foreach (var condition in conditions)
+        {
+            condition(exception);
+        }
+    }
+}
